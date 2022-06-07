5f878f54ef0432422eafd784d32109fd782b549e
Mateus Tavares <mateustm17@gmail.com>
2022-06-06 22:04:10 -0300

    Get first data from GoG

--- config/database.js
+++ config/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'wongames'),
-        username: env('DATABASE_USERNAME', 'wongames'),
-        password: env('DATABASE_PASSWORD', 'wongames123'),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi123'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
